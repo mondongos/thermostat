92f38df71f190b9554c8b846a7d35a055ad11853
Laurence <ltaylorapp@gmail.com>
2019-05-30 16:28:46 +0100

    Closing business logic & index.html

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -26,4 +26,38 @@ describe('Thermostat', function() {
     while(thermostat._temperature < thermostat._max) {thermostat.up();}
     expect(function() {thermostat.up()}).toThrowError("Ouch, my bones are hot! Owie!")
   });
+
+  it("max temperature should be 32 when power saving mode is off", function() {
+    thermostat.toggle();
+    expect(thermostat._max).toEqual(32);
+  });
+
+  it("max temperature should be 25 when power saving mode is on", function() {
+    expect(thermostat._max).toEqual(25);
+  });
+
+  it("temp can be reset to 20 using reset function", function() {
+    while(thermostat._temperature > thermostat._min) {thermostat.down();}
+    thermostat.reset();
+    expect(thermostat._temperature).toEqual(20)
+  });
+
+  it("lets user know current energy usage is low-usage", function(){
+    for(var i=0; i < 3; i++){
+      thermostat.down()
+    }
+    expect(thermostat.usage()).toEqual("Low")
+  });
+
+  it("lets user know current energy usage is low-usage", function(){
+    expect(thermostat.usage()).toEqual("Med")
+  });
+
+  it("lets user know current energy usage is low-usage", function(){
+    for(var i=0; i < 5; i++){
+      thermostat.up()
+    }
+    expect(thermostat.usage()).toEqual("High")
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,8 +1,8 @@
 function Thermostat() {
   this._temperature = 20;
-  this._min = 10
-  this._max = 25
-
+  this._min = 10;
+  this._max = 25;
+  this._powerSavingMode = true;
 };
 
 Thermostat.prototype.up = function() {
@@ -14,3 +14,18 @@ Thermostat.prototype.down = function() {
   if(this._temperature === this._min) {throw new Error("It's hecking freezing in here!")}
   this._temperature--;
 };
+
+Thermostat.prototype.toggle = function() {
+  this._powerSavingMode = !this._powerSavingMode;
+  this._powerSavingMode === true ? this._max = 25 : this._max = 32;
+}
+
+Thermostat.prototype.reset = function() {
+  this._temperature = 20
+}
+
+Thermostat.prototype.usage = function() {
+  if(this._temperature < 18) {return "Low"}
+  if(this._temperature < 25) {return "Med"}
+  if(this._temperature > 24) {return "High"}
+}
